41bf5029921b5c50eecc44223b54106934e565ed
Ignacio Iglesias <ignacio.iglesias@hotmail.es>
2016-06-06 02:33:11 +0200

    Add files via upload

---
+++ commands.sh
@@ -0,0 +1,91 @@
+#!/bin/bash
+# Edit your commands in this file.
+
+# This file is public domain in the USA and all free countries.
+# Elsewhere, consider it to be WTFPLv2. (wtfpl.net/txt/copying)
+
+if [ "$1" = "source" ];then
+	# Place the token in the token file
+	TOKEN=$(cat token)
+	# Set INLINE to 1 in order to receive inline queries.
+	# To enable this option in your bot, send the /setinline command to @BotFather.
+	INLINE=0
+	# Set to .* to allow sending files from all locations
+	FILE_REGEX='/home/user/allowed/.*'
+else
+	if ! tmux ls | grep -v send | grep -q $copname; then
+		[ ! -z ${URLS[*]} ] && {
+		curl -s ${URLS[*]} -o $NAME
+			send_file "${CHAT[ID]}" "$NAME" "$CAPTION"
+			rm "$NAME"
+		}
+		[ ! -z ${LOCATION[*]} ] && send_location "${CHAT[ID]}" "${LOCATION[LATITUDE]}" "${LOCATION[LONGITUDE]}"
+
+		# Inline
+		if [ $INLINE == 1 ]; then
+			# inline query data
+			iUSER[FIRST_NAME]=$(echo "$res" | sed 's/^.*\(first_name.*\)/\1/g' | cut -d '"' -f3 | tail -1)
+			iUSER[LAST_NAME]=$(echo "$res" | sed 's/^.*\(last_name.*\)/\1/g' | cut -d '"' -f3)
+			iUSER[USERNAME]=$(echo "$res" | sed 's/^.*\(username.*\)/\1/g' | cut -d '"' -f3 | tail -1)
+			iQUERY_ID=$(echo "$res" | sed 's/^.*\(inline_query.*\)/\1/g' | cut -d '"' -f5 | tail -1)
+			iQUERY_MSG=$(echo "$res" | sed 's/^.*\(inline_query.*\)/\1/g' | cut -d '"' -f5 | tail -6 | head -1)
+
+			# Inline examples
+			if [[ $iQUERY_MSG == photo ]]; then
+				answer_inline_query "$iQUERY_ID" "photo" "http://blog.techhysahil.com/wp-content/uploads/2016/01/Bash_Scripting.jpeg" "http://blog.techhysahil.com/wp-content/uploads/2016/01/Bash_Scripting.jpeg"
+			fi
+
+			if [[ $iQUERY_MSG == sticker ]]; then
+				answer_inline_query "$iQUERY_ID" "cached_sticker" "BQADBAAD_QEAAiSFLwABWSYyiuj-g4AC"
+			fi
+
+			if [[ $iQUERY_MSG == gif ]]; then
+				answer_inline_query "$iQUERY_ID" "cached_gif" "BQADBAADIwYAAmwsDAABlIia56QGP0YC"
+			fi
+			if [[ $iQUERY_MSG == web ]]; then
+				answer_inline_query "$iQUERY_ID" "article" "GitHub" "http://github.com/topkecleon/telegram-bot-bash"
+			fi
+		fi
+	fi
+	case $MESSAGE in
+		'/question')
+			startproc "./question"
+			;;
+		'/info')
+			send_markdown_message "${CHAT[ID]}" "This is bashbot, the *Telegram* bot written entirely in *bash*."
+			;;
+		'/start')
+			send_action "${CHAT[ID]}" "typing"
+			send_markdown_message "${CHAT[ID]}" "This is bashbot, the Telegram bot written entirely in bash.
+It features background tasks and interactive chats, and can serve as an interface for CLI programs.
+It currently can send, recieve and forward messages, custom keyboards, photos, audio, voice, documents, locations and video files.
+*Available commands*:
+*• /start*: _Start bot and get this message_.
+*• /info*: _Get shorter info message about this bot_.
+*• /question*: _Start interactive chat_.
+*• /cancel*: _Cancel any currently running interactive chats_.
+*• /kickme*: _You will be autokicked from the chat_.
+*• /leavechat*: _The bot will leave the group with this command _.
+Written by Drew (@topkecleon) and Daniil Gentili (@danogentili).
+Get the code in my [GitHub](http://github.com/topkecleon/telegram-bot-bash)
+"
+			;;
+			
+		'/leavechat')
+			send_markdown_message "${CHAT[ID]}" "*CHAT LEAVED*"
+   			leave_chat "${CHAT[ID]}"
+     			;;
+     			
+     		'/kickme')
+     			kick_chat_member "${CHAT[ID]}" "${USER[ID]}"
+     			unban_chat_member "${CHAT[ID]}" "${USER[ID]}"
+     			;;
+     			
+		'/cancel')
+			if tmux ls | grep -q $copname; then killproc && send_message "${USER[ID]}" "Command canceled.";else send_message "${USER[ID]}" "No command is currently running.";fi
+			;;
+		*)
+			if tmux ls | grep -v send | grep -q $copname;then inproc; else send_message "${USER[ID]}" "$MESSAGE" "safe";fi
+			;;
+	esac
+fi
