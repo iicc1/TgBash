3c372f73cce82412e0b7a67c152d4923be957ff2
jarriztg <jarriztg@users.noreply.github.com>
2016-06-12 22:49:58 -0500

    upps iicc :P

--- commands.sh
+++ commands.sh
@@ -179,13 +179,13 @@ ${MEMBERS[COUNT]}"
 			unban_chat_member "${CHAT[ID]}" "${REPLY[ID]}"
 			;;
 			
-		'/gban'
+		'/gban')
 			date=$(date)
 			echo "[$date --> ${REPLY[FIRST_NAME]} ${REPLY[LAST_NAME]} @${REPLY[USERNAME]} (${REPLY[ID]})]" >> settings/gbans
 			send_markdown_message "${CHAT[ID]}" "ID: ${REPLY[ID]} *globally banned.*"
 			;;
 
-		'/ungban'
+		'/ungban')
 			sed -i "/${REPLY[ID]}/d" settings/gbans
 			send_markdown_message "${CHAT[ID]}" "ID: ${REPLY[ID]} *globally unbanned*."
 			;;
