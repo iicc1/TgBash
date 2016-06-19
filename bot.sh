b0c842b5942c89da28c9bbcef99a102d71be3820
jarriztg <jarriztg@users.noreply.github.com>
2016-06-19 14:28:58 -0500

    change api file

--- bot.sh
+++ bot.sh
@@ -71,7 +71,7 @@ ME_URL=$URL'/getMe'
 ME=$(curl -s $ME_URL | ./JSON.sh/JSON.sh -s | egrep '\["result","username"\]' | cut -f 2 | cut -d '"' -f 2)
 
 
-FILE_URL='https://api.telegram.org/file/bot'$TOKEN'/'
+FILE_URL='https://api.pwrtelegram.xyz/file/bot'$TOKEN'/'
 UPD_URL=$URL'/getUpdates?offset='
 GET_URL=$URL'/getFile'
 OFFSET=0
