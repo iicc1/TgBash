abb63e967ec8ae2d8db309cf2793cfc63d7256a3
jarriztg <jarriztg@users.noreply.github.com>
2016-06-25 01:15:24 -0500

    uppss some forget

--- bot.sh
+++ bot.sh
@@ -134,20 +134,24 @@ send_text() {
 
 send_register() {
 	if [ "${CHAT[TYPE]}" == "private" ]; then
+	 if [ "$MESSAGE" ]; then
 	   echo -en '\n'
 	   echo -e '\e[1;37m'$MESSAGE'\e[0m'
 	   echo -en '\n'
 	   echo -e '\e[1;34mPRIVATE\e[0m || \e[1;37mID\e[0m \e[0;36m'${USER[ID]}'\e[0m | \e[1;37mUser\e[0m \e[0;36m@'${USER[USERNAME]}'\e[0m | \e[1;37mFirstname\e[0m \e[0;36m'${USER[FIRST_NAME]} '\e[0m | \e[1;37mLastname\e[0m \e[0;36m'${USER[LAST_NAME]}'\e[0m'
 	   echo ${DATE[ALL]}
+	 fi
  	fi
  	
- 	if [ "${CHAT[TYPE]}" != "private" ] | [[ "$MESSAGE" ]]; then
+ 	if [ "${CHAT[TYPE]}" != "private" ]; then
+ 	 if [ "$MESSAGE" ]; then
 	   echo -en '\n'
 	   echo -e '\e[1;37m'$MESSAGE'\e[0m'
 	   echo -en '\n'
 	   echo -e '\e[1;34mCHAT\e[0m || \e[1;37mID\e[0m \e[0;36m'${CHAT[ID]}'\e[0m | \e[1;37mTitle\e[0m \e[0;36m'${CHAT[TITLE]} '\e[0m'
 	   echo -e '\e[1;34mFROM\e[0m || \e[1;37mID\e[0m \e[0;36m'${USER[ID]}'\e[0m | \e[1;37mUser\e[0m \e[0;36m@'${USER[USERNAME]}'\e[0m | \e[1;37mFirstname\e[0m \e[0;36m'${USER[FIRST_NAME]} '\e[0m | \e[1;37mLastname\e[0m \e[0;36m'${USER[LAST_NAME]}'\e[0m'
 	   echo ${DATE[ALL]}
+	 fi
 	fi
 	
 	if [ "$NEW_MEMBER" ]; then
