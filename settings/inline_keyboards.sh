117a241c0338e91fe2fdc8f264367cb08720b775
jarriztg <jarriztg@users.noreply.github.com>
2016-06-25 02:36:41 -0500

    upss

--- settings/inline_keyboards.sh
+++ settings/inline_keyboards.sh
@@ -8,14 +8,15 @@ inline_keyboard[help]='{"inline_keyboard":[
 			]}'
 
 inline_keyboard[example1]='{"inline_keyboard":[
-			[{"text":"JUST A KEYBOARD","url":"'https://google.com'"}]}'
-			
-inline_keyboard[example2]='{"inline_keyboard":[
-			[{"text":"KEYBOARD 1","url":"'https://google.com'"}],
-			[{"text":"OTHER KEYBOARD LEFT","url":"'https://google.com'"}]
+			[{"text":"JUST A KEYBOARD","url":"'https://google.com'"}]
 			]}'
 			
+inline_keyboard[example2]='{"inline_keyboard":[
+ 			[{"text":"UP","url":"'https://google.com'"}],
+ 			[{"text":"DOWN","url":"'https://google.com'"}]
+ 			]}'
+ 
 inline_keyboard[example3]='{"inline_keyboard":[
-			[{"text":"KEYBOARD 1 -","url":"'https://google.com'"},
-			{"text":"OTHER KEYBOARD DOWN","url":"'https://google.com'"}]
+			[{"text":"LEFT","url":"'https://google.com'"},
+			{"text":"RIGHT","url":"'https://google.com'"}]
 			]}'
