41787f593ebfad2d4a586700754983989bb82aa4
jarriztg <jarriztg@users.noreply.github.com>
2016-06-27 03:26:12 -0500

    Update export_gbans.sh

--- export_gbans.sh
+++ export_gbans.sh
@@ -1,11 +1,14 @@
+#!/bin/bash
 
 if [ "$1" ]; then
-echo $1
-declare -a DATE
-DATE=$(date +%F)
-cat $1 | grep -E [0-9] |  sed 's/    /['${DATE}' --> EXPORTED (/g' | sed 's/,/)]/g' >> settings/gbans
+  clear
+  declare -a DATE
+  DATE=$(date +%F)
+  cat $1 | grep -E [0-9] |  sed 's/    /['${DATE}' --> EXPORTED (/g' | sed 's/,/)]/g' >> settings/gbans
+  echo -e '\e[0;32mList exported sucessfully\e[0m' || echo -e '\e[0;31mError. The file not exist.\e[0m'
 fi
 
 if [ ! "$1" ]; then
-echo 'ERROR. You need set the file then'
+  clear
+  echo -e '\e[0;31mError. You need set the filename then\e[0m'
 fi
