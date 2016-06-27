c95e72d0a37fa622a73467ad612d3a6cf89d1d35
root <root@vps273405.ovh.net>
2016-06-27 03:16:29 +0200

    adding export gbans from DBTeam

---
+++ export_gbans.sh
@@ -0,0 +1,12 @@
+
+if [ "$1" ]; then
+echo $1
+declare -a DATE
+DATE=$(date +%F)
+cat $1 | grep -E [0-9] |  sed 's/    /['${DATE}' --> EXPORTED (/g' | sed 's/,/)]/g' >> gbans
+echo ")]" | sed 's/\n//g' >> gbans
+fi
+
+if [ ! "$1" ]; then
+echo 'ERROR. You need set the file then'
+fi
