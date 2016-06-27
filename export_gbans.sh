3779552481c1809346e27a315b73df3e9b4a25ce
jarriztg <jarriztg@users.noreply.github.com>
2016-06-27 03:21:33 -0500

    Update export_gbans.sh

--- export_gbans.sh
+++ export_gbans.sh
@@ -3,7 +3,7 @@ if [ "$1" ]; then
 echo $1
 declare -a DATE
 DATE=$(date +%F)
-cat $1 | grep -E [0-9] |  sed 's/    /['${DATE}' --> EXPORTED (/g' | sed 's/,/)]/g' >> gbans
+cat $1 | grep -E [0-9] |  sed 's/    /['${DATE}' --> EXPORTED (/g' | sed 's/,/)]/g' >> settings/gbans
 fi
 
 if [ ! "$1" ]; then
