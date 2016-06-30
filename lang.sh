090bf65aad6fa5c42f446f208b5ff518f8360046
Harry <jarriztg@users.noreply.github.com>
2016-06-30 02:54:39 -0500

    some langs added and members count changed

--- lang.sh
+++ lang.sh
@@ -41,7 +41,7 @@ Más funciones hechas por @iicc1 y @Jarriz.
   
 *Basado* en [telegram-bot-bash](http://github.com/topkecleon/telegram-bot-bash)'
   
-  lang[INFO]='*Este es un bot de *Telegram* escrito completamente en bash.
+  lang[INFO]='Este es un bot de *Telegram* escrito completamente en bash.
 Obtén el código aquí [here](https://github.com/iicc1/TgBash)'
 
   lang[USER]='Usuario'
